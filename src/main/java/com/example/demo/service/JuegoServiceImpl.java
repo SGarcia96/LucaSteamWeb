9a6d81a851306af5d902b21732db82518fe19b1d
Josema017 <jmom1991@gmail.com>
2022-01-13 19:02:46 +0100

    find2

--- src/main/java/com/example/demo/service/JuegoServiceImpl.java
+++ src/main/java/com/example/demo/service/JuegoServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ejemplos.spring.model.User;
 import com.example.demo.dao.JuegoDAO;
 import com.example.demo.model.Juego;
 
