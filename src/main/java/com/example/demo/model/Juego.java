4b8f7958abcedb0064fcd910829473b887c0ea22
EvaMontiel <evamontielaraque@gmail.com>
2022-01-13 17:10:32 +0100

    entity juego

--- src/main/java/com/example/demo/model/Juego.java
+++ src/main/java/com/example/demo/model/Juego.java
@@ -1,5 +1,38 @@
 package com.example.demo.model;
 
-public class Juego {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@ToString
+public class Juego implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Getter @Setter private int id;
+	
+	@Getter @Setter String nombre;
+	@Getter @Setter private int fecha;
+	@Getter @Setter private String editor;
+	@Getter @Setter private String genero;
+	@Getter @Setter private String plataforma;
+	@Getter @Setter private double eu_sales;
+	@Getter @Setter private String fabricante;
+	
+	public Juego() {
+		super();
+	}
+	
 }
+
+
